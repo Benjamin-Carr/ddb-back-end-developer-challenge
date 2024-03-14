1fd97cd117097f85e559967cffa8eb8fa4444a05
Benjamin-Carr <37458984+Benjamin-Carr@users.noreply.github.com>
2024-03-14 00:24:04 -0400

    Built initial POST /character/{id}/damage API

--- hit-point-service/src/main/java/com/example/resources/CharacterResource.java
+++ hit-point-service/src/main/java/com/example/resources/CharacterResource.java
@@ -4,10 +4,7 @@ import com.example.api.ApiResponse;
 import com.example.api.Damage;
 import com.example.api.HitPointsResponse;
 import com.example.services.CharacterService;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/character/{characterId}")
@@ -17,12 +14,13 @@ public class CharacterResource {
     private final CharacterService characterService = new CharacterService();
     @POST
     @Path("/damage")
-    public ApiResponse<HitPointsResponse> damage(Damage damage) {
+    public ApiResponse<HitPointsResponse> damage(@PathParam("characterId") int characterId, Damage damage) {
+        HitPointsResponse data = characterService.dealDamage(characterId, damage);
         return ApiResponse.<HitPointsResponse>builder()
                 .id(12345)
                 .success(true)
                 .message("Hit points updated. Dealt " + damage.getAmount() + " points of " + damage.getDamageType() + " damage.")
-                .data(characterService.stub())
+                .data(data)
                 .build();
     }
 }
--- hit-point-service/src/main/java/com/example/services/CharacterService.java
+++ hit-point-service/src/main/java/com/example/services/CharacterService.java
@@ -1,6 +1,13 @@
 package com.example.services;
 
+import com.example.api.Damage;
 import com.example.api.HitPointsResponse;
+import com.example.core.Character;
+import com.example.db.CharacterDAO;
+import com.example.db.CharacterDaoInMemoryImpl;
+
+import static java.lang.Math.min;
+
 public class CharacterService {
 
     public HitPointsResponse stub() {
@@ -10,6 +17,45 @@ public class CharacterService {
                 .currentHitPointsDelta(-5)
                 .tempHitPoints(0)
                 .tempHitPointsDelta(-3)
+                .build();
+    }
+
+    public HitPointsResponse dealDamage(int characterId, Damage damage) {
+        CharacterDAO characterDAO = CharacterDaoInMemoryImpl.getInstance();
+        Character character = characterDAO.findCharacterById(characterId);
+
+        HitPointsResponse hitPointsResponse = calculateDamage(character, damage);
+
+        character.setTempHitPoints(hitPointsResponse.getTempHitPoints());
+        character.setCurrentHitPoints(hitPointsResponse.getCurrentHitPoints());
+
+        characterDAO.updateCharacter(character);
+
+        return hitPointsResponse;
+    }
+
+    private static HitPointsResponse calculateDamage(Character character, Damage damage) {
+        int damageAmount = damage.getAmount();
+
+        // deal with temporary HP
+        final int tempHpOriginal = character.getTempHitPoints();
+        final int tempHpLost = min(damageAmount, tempHpOriginal);
+        final int tempHpRemaining = tempHpOriginal - tempHpLost;
+        damageAmount -= tempHpLost;
+
+        // deal with regular HP
+        final int hpOriginal = character.getCurrentHitPoints();
+        final int hpLost = min(damageAmount, hpOriginal);
+        final int hpRemaining = hpOriginal - hpLost;
+        damageAmount -= hpLost;
+
+        return HitPointsResponse.builder()
+                .maxHitPoints(character.getMaxHitPoints())
+                .tempHitPoints(tempHpRemaining)
+                .tempHitPointsDelta(tempHpLost * -1)
+                .currentHitPoints(hpRemaining)
+                .currentHitPointsDelta(hpLost * -1)
+                .overflow(damageAmount)
                 .build();
     }
 
