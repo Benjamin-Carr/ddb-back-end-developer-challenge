f6caa88814d00b522c69a84c21029e1da4061dbb
Benjamin-Carr <37458984+Benjamin-Carr@users.noreply.github.com>
2024-03-17 05:00:57 -0400

    Updated BrivUtils to read deserialize case-insensitive enums

--- hit-point-service/src/main/java/com/example/services/BrivUtils.java
+++ hit-point-service/src/main/java/com/example/services/BrivUtils.java
@@ -1,29 +1,27 @@
 package com.example.services;
 
 import com.example.core.Character;
-import com.example.db.CharacterDaoInMemoryImpl;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class BrivUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(BrivUtils.class);
+
     public static final int BRIV_ID = 1;
 
     public static Character getBrivData() {
         try {
-            return new ObjectMapper().readValue(new File("src/main/resources/briv.json"), Character.class);
-//            return new Character();
+            ObjectMapper objectMapper = new ObjectMapper().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+            return objectMapper.readValue(new File("src/main/resources/briv.json"), Character.class);
         } catch (Exception e) {
-            System.out.println(e);
+            logger.error(e.getMessage());
             // TODO handle exception better
             return null;
         }
-    }
-
-    private void resetStoredBriv() {
-        Character briv = BrivUtils.getBrivData();
-        briv.setId(BRIV_ID);
-        CharacterDaoInMemoryImpl.getInstance().updateCharacter(briv);
     }
 }
