590e73b636c7cbae9d147b175f0898aed4479f19
Benjamin-Carr <37458984+Benjamin-Carr@users.noreply.github.com>
2024-03-18 11:13:28 -0400

    added javadocs for HitPointsResponse

--- hit-point-service/src/main/java/com/example/api/HitPointsResponse.java
+++ hit-point-service/src/main/java/com/example/api/HitPointsResponse.java
@@ -15,10 +15,25 @@ import lombok.ToString;
 public class HitPointsResponse {
     int maxHitPoints;
     int tempHitPoints;
+    /**
+     * The amount the temporary hit points changed
+     */
     int tempHitPointsDelta;
     int currentHitPoints;
+    /**
+     * The amount the current hit points changed
+     */
     int currentHitPointsDelta;
+    /**
+     * Factor by which the input amount was adjusted before affecting the character.
+     * Usually this will be 1, but if a character is resistant to the damage type, it will be 0.5, for example.
+     */
     float multiplier;
+    /**
+     * Number of hit points in excess of the change, either positive or negative.
+     * For example, if a character is dealt more damage than they have hit points, this field will contain the remainder.
+     * Likewise for if a character is healed more than they were missing hit points.
+     */
     int overflow;
 
     public static HitPointsResponse of(Character character) {
