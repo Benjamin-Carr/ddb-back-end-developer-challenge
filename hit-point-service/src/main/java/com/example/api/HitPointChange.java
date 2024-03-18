69c7003b6c1d52545b724bd2eee8c460a48f4717
Benjamin-Carr <37458984+Benjamin-Carr@users.noreply.github.com>
2024-03-18 10:41:02 -0400

    removed unused validation function from HitPointChange

--- hit-point-service/src/main/java/com/example/api/HitPointChange.java
+++ hit-point-service/src/main/java/com/example/api/HitPointChange.java
@@ -1,8 +1,6 @@
 package com.example.api;
 
 import com.example.core.enums.DamageType;
-import com.example.services.DamageValidator;
-import jakarta.validation.constraints.AssertTrue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,11 +15,4 @@ import lombok.ToString;
 public class HitPointChange {
     private int amount;
     private DamageType damageType;
-
-    @AssertTrue(message = "damageType must not be null when amount is negative")
-    // TODO method should be named isValid but Hibernator prepends the message with the method name after the "is" part,
-    //  so the correct name makes the message look bad.
-    boolean isValidation() {
-        return DamageValidator.isValid(this);
-    }
 }
